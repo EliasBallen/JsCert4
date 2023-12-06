67fffa0c893aafc6626ce32b8fa8a2e12df47061
EliasBallen <@EliasBallen>
2023-12-06 11:41:55 -0500

    added post request functionality on mongoose find and create users and exercices

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const {connectDb,createUser,findbyname} = require('./mongoqueries.js');
+const {connectDb,createUser,findbyname,findUserbyId, createExercise,searchExercices} = require('./mongoqueries.js');
 connectDb();
 const express = require('express');
 const cors = require('cors');
@@ -46,22 +46,74 @@ app.post('/api/users/',async (req,res)=>{
     }
 })
 
-app.post('/api/users/:_id/exercises',(req,res)=>{
-    console.log(req.body.username);
+app.post('/api/users/:_id/exercises', async(req,res)=>{
+    //console.log(req.body.username);
+    const regex = /^[0-9]+$/
+    if(!req.body.description||req.body.description===""||!regex.test(req.body.duration)){
+        res.json({error:"no description or duration added"})    
+        return
+    }
+    console.log(req.body._id)
+    const username = await findUserbyId(req.params._id,done)
+    if(!username){
+        res.json({error:"no username asociated with the id"})
+        return
+    }    
+
+    let date = new Date(req.body.date)
+    if(date.toString() === 'Invalid Date'){
+        date = new Date()
+    }        
+    const timedate = date.getTime() 
 
     const reqjson = {
         _id: req.params._id,
         description: req.body.description,
-        duration:req.body.duration,
-        date:req.body.date
+        duration:parseInt(req.body.duration),
+        date:timedate
     }
-    res.json(reqjson)
+    const data = await createExercise(reqjson,done)
+    const resJson = {
+        username:username.username,
+        description:data.description,
+        duration:data.duration,
+        date: data.date.toDateString(),
+        _id:username._id,
+    }
+    res.json(resJson)
 })
+//leondaat
+//656e52fc4e9744fcef8ff036
 //https://exercise-tracker.freecodecamp.rocks/api/users/656dfb98c2e63b0014b067b0/logs?from=2023-5-1&to=2023-11-29&limit=1
 //{ from: '2023-5-1', to: '2023-11-29', limit: '1' }
-app.get('/api/users/:_id/logs?',(req,res)=>{
-    console.log(req.query)
+app.get('/api/users/:_id/logs?',async(req,res)=>{
+    //console.log(req.query)
+    const _id =req.params._id
+    const username = await findUserbyId(_id,done)
+    if(!username){
+        res.json({error:"no username asociated with the id"})
+        return
+    }  
+    const {from,to,limit} = req.query
+    const exercises = await searchExercices({from,to,limit,_id},done)
+
+    const resJson = {
+        username:username.username,
+        count:exercises.length,
+        _id: username._id,
+        log:exercises.map((e)=>{
+            return {
+                description:e.description,
+                duration:e.duration, 
+                date:e.date.toDateString()}
+        })
+    }
+    //console.log(exercises, exercises.length)
+    res.json(resJson)
 })
+
+
+
 
 function done(err=null,data=null){
     console.log(err||data)
--- mongoqueries.js
+++ mongoqueries.js
@@ -20,7 +20,7 @@ const exerciseSchema = new mongoose.Schema(
             require:true
         },
         duration:{
-            type:String,
+            type:Number,
             require:true
         },
         date:{
@@ -36,20 +36,63 @@ function connectDb(){
     mongoose.connect(process.env.MONGO_URI);
 }
 
+async function searchExercices({from,to,limit,_id},done){
+    let fromdate = new Date(from)
+    if(fromdate.toString() === 'Invalid Date'){
+        console.log("Not valid From argument " + new Date(0).getTime())
+        fromdate = new Date(0)
+    }
+    let todate = new Date(to)
+    let asignto = true
+    if(todate.toString() === 'Invalid Date'){        
+        asignto = false        
+        console.log("Not valid to argument")
+    } 
+    if(typeof limit === "string"){
+        if(!isNaN(limit))console.log("isNumber")
+        const exercises = await Exercise.find({
+            UserId: _id,
+            date: asignto?{$gte:fromdate,$lte:todate}:{$gte:fromdate}    
+        }).limit(parseInt(limit))
+        return exercises            
+    }
+    const exercises = await Exercise.find({
+        UserId: _id,
+        date: asignto?{$gte:fromdate,$lte:todate}:{$gte:fromdate}    
+    })
+    return exercises
+}
+
+async function createExercise({_id,description,duration,date},done){
+    console.log({_id,description,duration,date})
+    let newExercise = new Exercise({
+        UserId:_id,
+        description:description,
+        duration: duration,
+        date: date
+    })
+    try {
+        const data = await newExercise.save()
+        const findedData = await Exercise.findById(data._id)
+        done(null,data)
+        return findedData
+    } catch (error) {
+        done(error)
+    }
+}
 async function createUser(username,done){
     let newUser = new User({
         username:username
     })
     try{
         const data = await newUser.save()
-        const findedData = await User.findOne({_id:data.id})
+        const findedData = await User.findById(data.id)
         done(null,findedData)
         return findedData
     } catch(err){
         done(err)
     }   
 }
-
 async function findbyname(username,done){
     try {
         const data = await User.findOne({username:username})
@@ -58,10 +101,19 @@ async function findbyname(username,done){
     } catch (error) {
         done(error)
     }
-
-
+}
+async function findUserbyId(userId,done){
+    try {
+        const data = await User.findById(userId)
+        done(null,data)
+        return data
+    } catch (error) {
+        done(error)
+    }
 }
 
-module.exports = {connectDb, createUser,findbyname}
+
+
+module.exports = {connectDb, createUser,findbyname,findUserbyId,createExercise,searchExercices}
 
 
